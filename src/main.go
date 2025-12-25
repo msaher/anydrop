761f3f24f0f4c72f6af8a978c666ae854bfa125f
saher <msaher.shair@gmail.com>
2025-12-25 22:38:17 +0300

    allow downloading arbitrary files

--- src/main.go
+++ src/main.go
@@ -5,10 +5,38 @@ import (
 	"net/http"
 	"net"
 	"os"
+	"flag"
 )
 
-func myIp(interfaceName string) (*net.IPNet, error) {
+type App struct {
+	downloadFileName string
+}
+
+func (app *App) download(w http.ResponseWriter, r *http.Request) {
+	fileName := app.downloadFileName
+	file, err := os.Open(fileName)
+	if err != nil {
+		w.Header().Set("Content-Type", "text/plain")
+		msg := fmt.Sprintf("not found: %s", fileName)
+		http.Error(w, msg, 404)
+		return
+	}
+	defer file.Close()
+
+	stat, err := file.Stat()
+	if err != nil {
+		http.Error(w, "stat failed", 500)
+		return
+	}
+
 	w.Header().Set("Cache-Control", "no-store")
+	attachment := fmt.Sprintf(`attachment; filename="%s"`, fileName)
+	w.Header().Set("Content-Disposition", attachment)
+	http.ServeContent(w, r, stat.Name(), stat.ModTime(), file)
+
+	return
+}
+
 func myIp() (*net.IPNet, error) {
 	// get first non-loopback address
 	ifaces, err := net.Interfaces()
@@ -45,34 +73,20 @@ func myIp() (*net.IPNet, error) {
 	return nil, fmt.Errorf("couldn't find an Ipv4 address")
 }
 
-func download(w http.ResponseWriter, r *http.Request) {
-	fileName := "spaghetti.jpeg"
-	file, err := os.Open(fileName)
-	if err != nil {
-		w.Header().Set("Content-Type", "text/plain")
-		msg := fmt.Sprintf("not found: %s", fileName)
-		http.Error(w, msg, 404)
-		return
-	}
-	defer file.Close()
-
-	stat, err := file.Stat()
-	if err != nil {
-		http.Error(w, "stat failed", 500)
-		return
-	}
-
-	attachment := fmt.Sprintf(`attachment; filename="%s"`, fileName)
-	w.Header().Set("Content-Disposition", attachment)
-	http.ServeContent(w, r, stat.Name(), stat.ModTime(), file)
-
-	return
-}
 
 func main() {
+	var app App
+
+	flag.StringVar(&app.downloadFileName, "download", "", "file to download on /download")
+	flag.Parse()
+
+	if app.downloadFileName == "" {
+		fmt.Fprintf(os.Stderr, "must specify a -download path")
+	}
+
 	// register handlers
 	mux := http.NewServeMux()
-	mux.HandleFunc("/download", download)
+	mux.HandleFunc("/download", app.download)
 
 	addr := ":8000"
 	server := http.Server {
